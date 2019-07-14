de0f7c06069c367c6bb07eb2dd99b0b187312e3e
Numakura <kazuma@example.comnew.pillow26@gmail.com>
2019-07-14 15:46:00 +0900

    no message

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
         :recoverable, :rememberable, :validatable
 
 has_one_attached :avatar
-
+has_many :posts
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :description,  presence: true, length: { maximum: 500 }
 end
--- db/migrate/20190714050057_add_user_id_to_posts.rb
+++ db/migrate/20190714050057_add_user_id_to_posts.rb
@@ -1,5 +1,5 @@
 class AddUserIdToPosts < ActiveRecord::Migration[5.2]
   def change
-    add_reference :posts, :user, foreign_key: true
+      add_reference :posts, :user, foreign_key: true, index: true, after: :category_id
   end
 end
